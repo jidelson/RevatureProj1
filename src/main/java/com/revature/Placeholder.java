293b70d9959789896eb4eed628496af7d87005f8
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-06-30 18:34:42 -0400

    initial commit

---
+++ src/main/java/com/revature/Placeholder.java
@@ -0,0 +1,8 @@
+package com.revature;
+
+public class Placeholder {
+	//this is a placeholder so that this project pushes correctly
+	
+	//Remember your project won't have a launcher!! 
+	//it'll have a master servlet though 
+}
---
+++ src/test/java/com/revature/Tests.java
@@ -0,0 +1,6 @@
+package com.revature;
+
+public class Tests {
+	//this is a placeholder test class - feel free to delete or rename it
+
+}
