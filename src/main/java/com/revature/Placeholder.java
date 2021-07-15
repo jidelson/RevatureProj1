e85793835a662a929590807f23cc3b8c23a582d1
Joe Idelson <joeidelson@gmail.com>
2021-07-14 19:15:59 -0700

    second commit

--- src/main/java/com/revature/Placeholder.java
+++ src/main/java/com/revature/Placeholder.java
@@ -4,5 +4,5 @@ public class Placeholder {
 	//this is a placeholder so that this project pushes correctly
 	
 	//Remember your project won't have a launcher!! 
-	//it'll have a master servlet though 
+	//it'll have a master servlet though !
 }
