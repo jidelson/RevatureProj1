58c29bb293b9b5afc3f85e15d8dc82d168afef3a
Joe Idelson <joeidelson@gmail.com>
2021-07-21 11:21:12 -0700

    added more tests

--- src/test/java/com/revature/Tests.java
+++ src/test/java/com/revature/Tests.java
@@ -2,17 +2,22 @@ package com.revature;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import com.revature.services.LoginService;
+import com.revature.services.ReimbursementsService;
 import com.revature.services.UsersService;
 
 public class Tests {
 
 	LoginService ls = new LoginService();
 	UsersService us = new UsersService();
+	ReimbursementsService rs = new ReimbursementsService();
+	
+// -------------------------------------------------------------------------------------------------
 	
 	@Test //we need the @Test annotation in order for this to be recognized as a testing method
 	public void testValidLogin() {
@@ -28,7 +33,7 @@ public class Tests {
 		assertFalse(ls.login("sfesf", "grsdgdg"));
 	}
 	
-	//---------------------------------------------------
+	//-----------------------------------------------------------------------------------------
 	
 	@Test
 	public void getUsersFirstName() {
@@ -36,10 +41,44 @@ public class Tests {
 		assertEquals(us.getUsers("jidelson").getUser_first_name(), "Joe");
 	}
 	
-	@Test public void getUsersLastName() {
+	@Test
+	public void notUsersFirstName() {
+		System.out.println("TESTING NOT EQUALS FIRST NAME");
+		assertNotEquals(us.getUsers("dfischer").getUser_first_name(), "Mike");
+	}
+	
+	@Test 
+	public void getUsersLastName() {
 		System.out.println("TESTING FOR GETTING USERS LAST NAME");
 		assertEquals(us.getUsers("jidelson").getUser_last_name(), "Idelson");
 	}
+	
+	@Test public void notUsersLastName() {
+		System.out.println("TESTING NOT EQUALS LAST NAME");
+		assertNotEquals(us.getUsers("chames").getUser_last_name(), "Smith");
+	}
+	
+	@Test
+	public void getUsersEmail() {
+		System.out.println("TESTING FOR USERS EMAIL");
+		assertEquals(us.getUsers("jidelson").getUser_email(), "joeidelson@gmail.com");
+	}
+	
+	@Test
+	public void notUsersEmail() {
+		System.out.println("TESTING NOT EQUALS EMAIL");
+		assertNotEquals(us.getUsers("klinares85").getUser_email(), "kenny@hotmail.com");
+	}
+	
+	@Test
+	public void getRoleId() {
+		System.out.println("TESTING FOR USERS ROLE ID");
+		assertEquals(us.getUsers("jidelson").getUser_role_id(), 1);
+	}
+	
+	
+	//-----------------------------------------------------------------------------------------
+	
 	
 	
 	
